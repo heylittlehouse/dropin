701a8515aafae8dcdf726c9f9d53a3631d63c433
heylittlehouse <michael.r.landon@gmail.com>
2016-01-06 12:47:23 -0800

    Add associations for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ActiveRecord::Base
+	belongs_to :host, class_name: 'User', foreign_key: :user_id
+	has_many :events_tags
+	has_many :tags, through: :events_tags
+	has_many :events_users
+	has_many :attendees, through: :events_users, class_name: 'User', foreign_key: :user_id
 end
