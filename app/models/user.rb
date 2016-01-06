125fc18c5ecbc421c303f095f64a91c556adee42
heylittlehouse <michael.r.landon@gmail.com>
2016-01-06 14:08:59 -0800

    Add colon to through

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 	has_many :events_users
 	has_many :attended_events, through: :events_users, class_name: 'Event'
 	has_many :tags_users
-	has_many :interests, through :tags_users, class_name: 'Tag'
+	has_many :interests, through: :tags_users, class_name: 'Tag'
 
 	has_secure_password
 end
