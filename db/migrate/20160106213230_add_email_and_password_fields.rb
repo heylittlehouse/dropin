bdbc1d3d163343a68d33c301f2942938316cc14f
heylittlehouse <michael.r.landon@gmail.com>
2016-01-06 13:39:12 -0800

    Add email and password columns to user

---
+++ db/migrate/20160106213230_add_email_and_password_fields.rb
@@ -0,0 +1,6 @@
+class AddEmailAndPasswordFields < ActiveRecord::Migration
+  def change
+  	add_column :users, :email, :string, null: false
+  	add_column :users, :password_digest, :string, null: false
+  end
+end
