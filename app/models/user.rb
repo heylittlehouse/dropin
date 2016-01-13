1738ba96f52d18573d999f5e7f6afd72e69b9be1
heylittlehouse <michael.r.landon@gmail.com>
2016-01-13 14:36:42 -0800

    Add user presence:true validations and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
+	has_secure_password
+
 	has_many :hosted_events, foreign_key: :user_id, class_name: "Event"
 	has_many :events_users
 	has_many :attended_events, through: :events_users, source: :event
 	has_many :tags_users
 	has_many :interests, through: :tags_users, source: :tag
 
-	has_secure_password
+	validates :first_name, :last_name, :email, :password_digest, presence: true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe User, type: :model do
     it {should have_many :interests}
   end
 
+  context "validations" do
+    it {should validate_presence_of :first_name}
+    it {should validate_presence_of :last_name}
+    it {should validate_presence_of :email}
+    it {should validate_presence_of :password_digest}
+  end
+
 end
