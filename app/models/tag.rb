33b9cb5983d9c5c2f26196acc86f42ab981eb7d6
heylittlehouse <michael.r.landon@gmail.com>
2016-01-13 16:59:09 -0800

    Add tag name presence and uniqueness validations and spec

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,7 @@ class Tag < ActiveRecord::Base
 	has_many :events, through: :events_tags
 	has_many :tags_users
 	has_many :users, through: :tags_users
+
+	validates :name, presence: true
+	validates :name, uniqueness: true
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -6,4 +6,14 @@ RSpec.describe Tag, type: :model do
     it {should have_many :users}
   end
 
+  context "validations" do
+  	context "presence" do
+	    it {should validate_presence_of :name}
+  	end
+
+  	context "uniqueness" do
+	    it {should validate_uniqueness_of :name}
+	  end
+  end
+
 end
